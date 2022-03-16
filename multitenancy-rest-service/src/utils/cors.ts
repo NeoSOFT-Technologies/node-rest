3795d7066cc3452af412000e47ae78d198562f02
Sumit <sumit.singh@neosofttech.com>
2022-03-16 16:04:03 +0530

    refactor: updated cors

--- multitenancy-rest-service/src/utils/cors.ts
+++ multitenancy-rest-service/src/utils/cors.ts
@@ -3,7 +3,7 @@ import { INestApplication } from "@nestjs/common";
 export function setupCors(app: INestApplication) {
     app.enableCors({
         origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', ' PATCH', 'DELETE'],
+        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
     })
 };

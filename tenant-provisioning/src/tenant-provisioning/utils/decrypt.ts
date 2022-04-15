34e6c636d474662bd4ec2903e7ec3ff201566062
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-04-15 18:46:50 +0530

    Add files via upload

--- tenant-provisioning/src/tenant-provisioning/utils/decrypt.ts
+++ tenant-provisioning/src/tenant-provisioning/utils/decrypt.ts
@@ -1,1 +1,16 @@
+import * as decrypt from 'crypto';
 
+const algorithm = "aes-256-cbc";
+
+// generate 16 bytes of random data
+const initVector = decrypt.randomBytes(16);
+
+// secret key generate 32 bytes of random data
+const Securitykey = decrypt.randomBytes(32);
+
+export function decodePassword(encryptedPassword: string){
+    const decipher = decrypt.createDecipheriv(algorithm, Securitykey, initVector);
+    let decryptedData = decipher.update(encryptedPassword, "hex", "utf-8");
+    decryptedData += decipher.final("utf8");
+    return decryptedData;
+}

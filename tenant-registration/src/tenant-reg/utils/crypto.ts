70afc67b01f07bbf18e70289ee52e063b5cc01c3
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-05-11 12:28:11 +0530

    Update crypto.ts

--- tenant-registration/src/tenant-reg/utils/crypto.ts
+++ tenant-registration/src/tenant-reg/utils/crypto.ts
@@ -1,17 +1,7 @@
-import * as crypto from 'crypto';
+import { hashSync } from 'bcryptjs';
 
-const algorithm = "aes-256-cbc";
+const SALT = 10;
 
-// generate 16 bytes of random data
-const initVector = crypto.randomBytes(16);
-
-// secret key generate 32 bytes of random data
-const Securitykey = crypto.randomBytes(32);
-
-
-export function encodePassword(rawPassword: string){
-    const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
-    let encryptedData = cipher.update(rawPassword, "utf-8", "hex");
-    encryptedData += cipher.final("hex");
-    return  encryptedData;
+export function encodePassword(rawPassword: string) {
+  return hashSync(rawPassword, SALT);
 }

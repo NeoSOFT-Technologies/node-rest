e6f38a6ec49c2e0c9b7a990920b72645ee1007a2
sarikadharmani <sarikadharmani@gmail.com>
2022-05-17 15:27:13 +0530

    commit on over-riding

--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
 CREATE USER nest;
 
 CREATE DATABASE IF NOT EXISTS rest_api;
+CREATE USER 'user'@'%';
 GRANT ALL PRIVILEGES ON rest_api.* TO 'user'@'%';
 GRANT ALL PRIVILEGES ON rest_api.* TO 'nest'@'%';

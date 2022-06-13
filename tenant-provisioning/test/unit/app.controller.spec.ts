16497e1528ae3339cc7b30b8a62f44050a587311
Sarika002 <sarika.dharmani@neosoftmail.com>
2022-06-13 14:04:47 +0530

    fix: added test cases

---
+++ tenant-provisioning/test/unit/app.controller.spec.ts
@@ -0,0 +1,34 @@
+import {AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { Test, TestingModule } from '@nestjs/testing';
+
+describe('Testing AppController', () => {
+    let appController: AppController;
+    let appService: AppService;
+
+    const mockAppService = {
+        getHello: jest.fn()
+    };
+
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AppController],
+            providers: [AppService],
+        })
+            .overrideProvider(AppService)
+            .useValue(mockAppService)
+            .compile();
+
+    appController = module.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
+    });
+
+    it('should return "Hello World!"', () => {
+        const message = 'hello-2';
+
+        const getHello = jest.spyOn(appService, 'getHello');
+        appController.getHello(message);
+        expect(getHello).toHaveBeenCalledWith(message);
+    });
+
+});
